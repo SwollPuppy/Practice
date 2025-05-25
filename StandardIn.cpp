bc7658c59e13bd4f6ee3a74db0b6441657ab280d
Bobby Hendrich <RHendrich@gmail.com>
2025-05-25 10:07:42 -0700

    Playing with string streams

--- StandardIn.cpp
+++ StandardIn.cpp
@@ -1,17 +1,66 @@
 // StandardIn.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include "StandardIn.h"
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <vector>
+
+void showStringVector(std::vector<std::string>& sList) {
+	if (sList.size() > 1) {
+		for (size_t i = 0; i < sList.size(); i++) {
+			std::cout << sList[i] << "  ";
+		}
+		std::cout << std::endl;
+	}
+}
 
 int main()
 {
 	std::string name;
+	std::string myList;
+	std::vector<std::string> result;
+	std::vector<std::string> meList;
+
+	std::stringstream ss("3,13,9,4,7");
 
     std::cout << "Hello World!\n";
 	std::cout << "Who are you?" << std::endl;
 	std::cin >> name;
 	std::cout << "Hello " << name << std::endl;
+
+	while (ss.good())
+	{
+		std::string substr;
+		std::getline(ss, substr, ',');
+		result.push_back(substr);
+	}
+
+	showStringVector(result);
+
+	std::cout << "Now you give me a string." << std::endl;
+	std::cin >> myList;
+	std::stringstream st(myList);
+
+	while (st.good())
+	{
+		std::string substr;
+		std::getline(st, substr, ',');
+		meList.push_back(substr);
+	}
+
+	showStringVector(meList);
+
+	//void showStringVector(std::vector<std::string>& sList) {
+	//	if (sList.size() > 1) {
+	//		for (size_t i = 0; i < sList.size(), i++) {
+	//			std::cout << sList[i] << "  ";
+	//		}
+	//		std::cout << std::endl;
+	//	}
+	//}
+
 	return 0;
 }
 
